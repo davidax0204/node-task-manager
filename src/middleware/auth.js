6de701af812066b1f100fd813f0e02fb83985b69
davidax0204 <davidax5625@gmail.com>
2021-04-25 18:03:02 +0300

    Read my profile works

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -6,10 +6,12 @@ const auth = async (req,res,next) =>
     try
     {
         const token = req.header('Authorization').replace('Bearer ','')
+        console.log("Token :",token)
         // from user - generateAuthToken
         const decoded = jwt.verify(token, 'thisismycourse')
-        console.log(decoded)
+        console.log("decoded " , decoded)
         const user = await User.findOne({ _id: decoded._id, 'tokens.token': token})
+        // console.log("User : ",user)
         if(!user)
         {
             // to trigger the catch
@@ -26,5 +28,32 @@ const auth = async (req,res,next) =>
         res.status(401).send({error: 'please authenticate'})
     }
 }
+
+// module.exports = auth
+
+// const jwt = require('jsonwebtoken')
+// const User = require('../models/user')
+
+// const auth = async(req,res,next) =>
+// {
+//     try
+//     {
+//         const token = req.header('Authorization').replace('Bearer ','')
+//         const decoded = jwt.verify(token, 'thisismycourse')
+//         // find a user with the correct id and has the same token stored
+//         const user = await User.findOne({ _id:decoded._id, 'tokens.token':token })
+//         if (!user)
+//         {
+//             throw new Error()
+//         }
+//         req.user = user
+
+//         next()
+//     }
+//     catch(e)
+//     {
+//         res.status(401).send({error:'please authenticate'})
+//     }
+// }
 
 module.exports = auth
