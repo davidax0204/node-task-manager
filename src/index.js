d59450895c49cca9481b53e3e3f225841bc33162
davidax0204 <davidax5625@gmail.com>
2021-04-28 16:21:14 +0300

    Chpater 14 done-serving images, express errors, file validation

--- src/index.js
+++ src/index.js
@@ -15,18 +15,4 @@ app.listen(port, ()=>
     console.log('server is on port '+ port)
 })
 
-const Task = require('./models/task')
-const User = require('./models/user')
 
-const main = async ()=>
-{
-    // const task = await Task.findById('6085a93d6f68e4153c6fdb82')
-    // await task.populate('owner').execPopulate()
-    // console.log(task.owner)
-
-    // const user = await User.findById('6085a788c5c4712e9cd3acc2')
-    // await user.populate('tasks').execPopulate()
-    // console.log(user.tasks)
-}
-
-main()
